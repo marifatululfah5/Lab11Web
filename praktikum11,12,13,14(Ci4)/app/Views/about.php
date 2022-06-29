70b3e8796ef2399b23bb1305c0e8ad1d2020a717
marifatululfah5 <marifatululfah99@gmail.com>
2022-06-29 21:41:51 +0700

    praktikum

---
+++ praktikum11,12,13,14(Ci4)/app/Views/about.php
@@ -0,0 +1,10 @@
+<?= $this->include('template/header'); ?>
+<section id="about">
+        <div class="row">
+            <img src="ullfah.jpg" title="Marifatul Ulfah" alt="Marifatul Ulfah" class="" width="200" style="float: left; border: 3px solid black;">
+            <h1>Marifatul Ulfah</h1>
+            <p>Perkenalkan nama saya Marifatul Ulfah. Saya mahasiswa Universitas Pelita Bangsa Angakatan tahun 2019. 
+            </p>
+        </div>
+    </section>
+<?= $this->include('template/footer'); ?>
